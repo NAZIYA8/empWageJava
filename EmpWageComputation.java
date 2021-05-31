7d6624f4bfd5007895d94db8485ecdcc470a713d
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-01 00:36:31 +0530

    calculated wages for a month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -7,12 +7,16 @@ public class EmpWageComputation {
 	public static void main(String[] args) {
 		final int IS_PART_TIME = 1;
 		final int IS_FULL_TIME = 2;
-		
+		int TotalSalary=0;
 		int Emp_Rate_Per_Hr = 20;
+		int Num_Working_Days =20;
 		int EmpHrs;
 		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-
+		int i;
+		for (i=1; i<= Num_Working_Days; i++) {
+			
 		System.out.println(empCheck);
+		
 		switch (empCheck) {
 		case IS_PART_TIME :
 			System.out.println("Empcheck is 1 (parttime)");
@@ -26,7 +30,10 @@ public class EmpWageComputation {
 			System.out.println("Empcheck is 0");
 			EmpHrs = 0;
 		}
-		double salary = (EmpHrs * Emp_Rate_Per_Hr);
-		System.out.println(salary);
+		int salary = (EmpHrs * Emp_Rate_Per_Hr);
+		 TotalSalary = (TotalSalary+salary);
+		System.out.println("Salary : "+ salary);
+		System.out.println("Total Salary : " + TotalSalary);
+		}
 	}
 }
