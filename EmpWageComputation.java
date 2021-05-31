59b37fe94718f91f7aa4a054b98b2c7df04696b7
NAZIYA8 <syeda.naziya896@gmail.com>
2021-05-31 22:44:15 +0530

    Added PartTime Employee

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,18 +2,23 @@ package EmpWage;
 
 public class EmpWageComputation {
 	public static void main(String[] args) {
-	
-		int IS_FULL_TIME  = 1;
-		double empCheck = Math.floor(Math.random()*10) % 2;	
-		
+
+		double IS_PART_TIME = 1;
+		double IS_FULL_TIME = 2;
+		double Emp_Rate_Per_Hr = 20;
+		double EmpHrs;
+
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+
 		if (empCheck == IS_FULL_TIME) {
-			double EmpRatePerHr = 20;
-			double EmpHrs = 8;
-			double salary = (EmpRatePerHr*EmpHrs);
-			System.out.println(salary);
+			EmpHrs = 12;
+		} else if (empCheck == IS_PART_TIME) {
+			EmpHrs = 8;
+		} else {
+			EmpHrs = 0;
 		}
-		else {
-				System.out.println("Salary is  0 ");
-			}
-   	}
+
+		double salary = (EmpHrs * Emp_Rate_Per_Hr);
+		System.out.println(salary);
+	}
 }
