4e98a421e03b5d0a997326d6dd196527dfa22947
NAZIYA8 <syeda.naziya896@gmail.com>
2021-05-31 21:27:01 +0530

    Added Wage Calculation

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -6,9 +6,14 @@ public class EmpWageComputation {
 		int IS_FULL_TIME  = 1;
 		double empCheck = Math.floor(Math.random()*10) % 2;	
 		
-		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present ");
-		else
-			System.out.println("Employee is Absent");	
-	}
+		if (empCheck == IS_FULL_TIME) {
+			double EmpRatePerHr = 20;
+			double EmpHrs = 8;
+			double salary = (EmpRatePerHr*EmpHrs);
+			System.out.println(salary);
+		}
+		else {
+				System.out.println("Salary is  0 ");
+			}
+   	}
 }
