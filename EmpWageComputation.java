487431ef41a1f01c43de59fbcf688111d5370881
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-01 17:32:05 +0530

    Refactored code-Number of working days 100

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,23 +1,26 @@
 package EmpWage;
 
 public class EmpWageComputation {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static int Max_Hrs_In_Month = 100;
+	public static int Num_Working_Days = 20;
+	public static int Emp_Rate_Per_Hr = 20;
+
 
 	public static void main(String[] args) {
-		final int IS_PART_TIME = 1;
-		final int IS_FULL_TIME = 2;
-		int Max_Hrs_In_Month = 100;
+		
+		
 		int TotalSalary = 0;
-		int Emp_Rate_Per_Hr = 20;
-		int Num_Working_Days = 20;
-		int EmpHrs;
+		int EmpHrs = 0;
 		int TotalEmpHrs = 0;
 		int TotalWorkingDays = 0;
 
 		while (TotalEmpHrs < Max_Hrs_In_Month && TotalWorkingDays < Num_Working_Days) {
 			TotalWorkingDays = TotalWorkingDays + 1;
-System.out.println(String.format("Day: %d", TotalWorkingDays));
-			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			System.out.println("Day:" + TotalWorkingDays);
 			
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case IS_PART_TIME:
 				System.out.println("Empcheck is 1 (parttime)");
@@ -34,7 +37,7 @@ System.out.println(String.format("Day: %d", TotalWorkingDays));
 			TotalEmpHrs = (TotalEmpHrs + EmpHrs);
 			System.out.println("Total employee hours:" + TotalEmpHrs);
 		}
-		TotalSalary = (TotalEmpHrs *  Emp_Rate_Per_Hr);
+		TotalSalary = (TotalEmpHrs * Emp_Rate_Per_Hr);
 		System.out.println("Total Salary : " + TotalSalary);
 	}
 }
